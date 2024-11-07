534ff40613bcb5cf10f2cebe76d8c1763a3643c4
Nis3D <103100755+Nis3D@users.noreply.github.com>
2024-11-07 23:29:42 +0800

    Try and catch execute to PDO instance inside using php

--- config/db.php
+++ config/db.php
@@ -6,4 +6,15 @@ $dbname='projectdatabase';
 
 //set database source name
 $dsn ='mysql:host=' .$host .'; dbname=' .$dbname;
+try{
+    //create a PDO instance
+    $pdo= new PDO ($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   // echo" This is Connected Successfully";
+
+} catch (PDOException $e) {
+    echo" Connection failed!:" . $e->getMessage();
+}
+
 ?>
